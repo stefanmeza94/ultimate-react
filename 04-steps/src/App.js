020aedc534c5466cd118d068e1d74a3781421f02
Stefn Meza <stefanmeza93@gmail.com>
2023-08-16 00:05:03 +0200

    Add state in info and started new project

---
+++ 04-steps/src/App.js
@@ -0,0 +1,48 @@
+const messages = [
+  "Learn React ❄",
+  "Apply for job 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const step = 1;
+
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+// nastaviti od 05 - creating a state variable with usestate hook

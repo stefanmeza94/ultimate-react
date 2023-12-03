18fa15fdc126e8c7f0485ce456af3a13da6a446a
Stefan Meza <stefanmeza93@gmail.com>
2023-12-03 23:22:30 +0100

    Add fist effect

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import InputSearch from "./components/InputSerach";
@@ -9,9 +9,17 @@ import Box from "./components/Box";
 import WatchedSummery from "./components/WatchedSummery";
 import WatchedMoviesList from "./components/WatchedMoviesList";
 
+const KEY = "f84fc31d";
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((res) => setMovies(res.Search));
+  }, []);
 
   return (
     <>
--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -7,7 +7,7 @@ import StarRating from "./components/StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} />
+    <App />
+    {/* <StarRating maxRating={10} /> */}
   </React.StrictMode>
 );

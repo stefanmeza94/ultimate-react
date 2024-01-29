ab22440b438ba4c25db8f66ee72325e00d564ecd
Stefan Meza <stefanmeza93@gmail.com>
2024-01-29 15:03:52 +0100

    Add custom hook useMovies

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Search from "./components/Serach";
 import Results from "./components/Result";
-import { tempMovieData, tempWatchedData } from "./data";
 import MovieList from "./components/MovieList";
 import Box from "./components/Box";
 import WatchedSummery from "./components/WatchedSummery";
@@ -11,7 +10,7 @@ import WatchedMoviesList from "./components/WatchedMoviesList";
 import Loader from "./components/Loader";
 import ErrorMessage from "./components/ErrorMessage";
 import MovieDetails from "./components/MovieDetails";
-import { KEY } from "./constants";
+import { useMovies } from "./hooks/useMovies";
 
 function getLocalStorage() {
   const watchedMovies = JSON.parse(localStorage.getItem("watched"));
@@ -20,11 +19,10 @@ function getLocalStorage() {
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
   const [watched, setWatched] = useState(getLocalStorage);
+
+  const { movies, isLoading, error } = useMovies(query);
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -45,47 +43,6 @@ export default function App() {
   useEffect(() => {
     localStorage.setItem("watched", JSON.stringify(watched));
   }, [watched]);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    async function fetchMovies() {
-      try {
-        setIsLoading(true);
-        setError("");
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, { signal: controller.signal });
-
-        if (!res.ok) {
-          throw new Error("Something went wrong with fetching movies");
-        }
-
-        const data = await res.json();
-        if (data.Response === "False") {
-          throw new Error("Movie not found!");
-        }
-        setMovies(data.Search);
-        setError("");
-      } catch (error) {
-        if (error.name !== "AbortError") {
-          setError(error.message);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    if (query.length < 3) {
-      setMovies([]);
-      setError("");
-      return;
-    }
-
-    handleCloseMovie();
-    fetchMovies();
-
-    return function () {
-      controller.abort();
-    };
-  }, [query]);
 
   return (
     <>

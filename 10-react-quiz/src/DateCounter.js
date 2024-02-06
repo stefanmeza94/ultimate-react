67678160c215f81d2b5fee70613d8101b6b0fb5d
Stefan Meza <stefanmeza93@gmail.com>
2024-02-06 23:43:22 +0100

    Add useReducer and react-quiz app started

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,15 +1,24 @@
 import { useReducer, useState } from 'react';
 
 function reducer(state, action) {
-  if (action.type === 'inc') return state + 1;
-  if (action.type === 'dec') return state - 1;
-  if (action.type === 'setCount') return action.payload;
+  switch (action.type) {
+    case 'inc':
+      return { ...state, count: state.count + 1 };
+    case 'dec':
+      return { ...state, count: state.count - 1 };
+    case 'setCount':
+      return { ...state, count: action.payload };
+    default:
+      throw new Error('Unknown error!');
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
+  const intialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, intialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
@@ -33,12 +42,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
@@ -64,4 +73,4 @@ function DateCounter() {
 }
 export default DateCounter;
 
-// nastavi od 188. Managing Related Pieces of State
+// nastavi od 188. Managing Related Pieces of State - 06:20

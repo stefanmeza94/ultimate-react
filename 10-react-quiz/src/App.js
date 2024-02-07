16a5c97195136943846953462e9231c02628e62d
Stefan Meza <stefanmeza93@gmail.com>
2024-02-07 23:39:14 +0100

    Started react quiz app

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,16 @@
-import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
+
+// nastavi od 191. Loading Questions from a Fake API

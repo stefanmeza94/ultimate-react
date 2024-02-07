a8b23af54ddb119b216bd4494eb415f63f68e768
Stefan Meza <stefanmeza93@gmail.com>
2024-02-07 16:52:10 +0100

    Add complete state update with useReducer

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,59 +1,55 @@
-import { useReducer, useState } from 'react';
+import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
 function reducer(state, action) {
   switch (action.type) {
-    case 'inc':
-      return { ...state, count: state.count + 1 };
-    case 'dec':
-      return { ...state, count: state.count - 1 };
-    case 'setCount':
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
       return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
     default:
-      throw new Error('Unknown error!');
+      throw new Error("Unknown error!");
   }
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  // const [step, setStep] = useState(1);
-  const intialState = { count: 0, step: 1 };
-  const [state, dispatch] = useReducer(reducer, intialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
   // This mutates the date object.
-  const date = new Date('june 21 2027');
+  const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({ type: 'dec' });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    dispatch({ type: 'inc' });
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    dispatch({ type: 'setCount', payload: Number(e.target.value) });
-    // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    // setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    // setCount(0);
-    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
-    <div className='counter'>
+    <div className="counter">
       <div>
-        <input type='range' min='0' max='10' value={step} onChange={defineStep} />
+        <input type="range" min="0" max="10" value={step} onChange={defineStep} />
         <span>{step}</span>
       </div>
 
@@ -73,4 +69,4 @@ function DateCounter() {
 }
 export default DateCounter;
 
-// nastavi od 188. Managing Related Pieces of State - 06:20
+// 189. Managing State With useReducer

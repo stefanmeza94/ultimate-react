56f32da6b7da0973f5575839e0267df5eedd95c6
Stefan Meza <stefanmeza93@gmail.com>
2024-02-08 22:15:10 +0100

    Add json-server package; Fetch questions from fake api; Add first two type for reducer

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,7 +1,40 @@
+import { useReducer, useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+  status: 'loading',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Unknown action: ' + action.type);
+  }
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:8000/questions');
+        const data = await response.json();
+
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className='app'>
       <Header />
@@ -13,4 +46,4 @@ export default function App() {
   );
 }
 
-// nastavi od 191. Loading Questions from a Fake API
+// nastavi od 192. Handling Loading, Error, and Ready Status

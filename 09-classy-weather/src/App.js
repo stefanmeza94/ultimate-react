f4d18626d3d488464e8f8177c2ea48653a59f567
Stefan Meza <stefanmeza93@gmail.com>
2024-02-23 16:09:08 +0100

    Add loading and error page

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -1,5 +1,37 @@
 import React from "react";
 
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], "☀️"],
+    [[1], "🌤"],
+    [[2], "⛅️"],
+    [[3], "☁️"],
+    [[45, 48], "🌫"],
+    [[51, 56, 61, 66, 80], "🌦"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
+    [[71, 73, 75, 77, 85, 86], "🌨"],
+    [[95], "🌩"],
+    [[96, 99], "⛈"],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return "NOT FOUND";
+  return icons.get(arr);
+}
+
+function convertToFlag(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
+function formatDay(dateStr) {
+  return new Intl.DateTimeFormat("en", {
+    weekday: "short",
+  }).format(new Date(dateStr));
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,10 +42,40 @@ class App extends React.Component {
     this.fetchWeather = this.fetchWeather.bind(this);
   }
 
-  fetchWeather() {
-    console.log("Loading data...");
-    console.log(this);
-  }
+  fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
+    try {
+      this.setState({ isLoading: true });
+
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error("Location not found");
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+
+      this.setState({
+        displayLocation: `${name} ${convertToFlag(country_code)}`,
+      });
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({ weather: weatherData.daily });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
 
   render() {
     return (
@@ -34,5 +96,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// nastavi od 179. Fetching Weather Data
--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,49 +1,61 @@
-import { useReducer, useEffect } from 'react';
-import Header from './Header';
-import Main from './Main';
+import { useReducer, useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
-  status: 'loading',
+  status: "loading",
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'dataReceived':
-      return { ...state, questions: action.payload, status: 'ready' };
-    case 'dataFailed':
-      return { ...state, status: 'error' };
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
-      throw new Error('Unknown action: ' + action.type);
+      throw new Error("Unknown action: " + action.type);
   }
 };
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const numQuestions = state.questions?.length;
+
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('http://localhost:8000/questions');
+        const response = await fetch("http://localhost:8000/questions");
         const data = await response.json();
 
-        dispatch({ type: 'dataReceived', payload: data });
+        dispatch({ type: "dataReceived", payload: data });
       } catch (error) {
-        dispatch({ type: 'dataFailed' });
+        dispatch({ type: "dataFailed" });
       }
     }
     fetchData();
   }, []);
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Questions</p>
+        {state.status === "loading" && <Loader />}
+        {state.status === "error" && <Error />}
+        {state.status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {state.status === "active" && <Question />}
       </Main>
     </div>
   );
 }
 
-// nastavi od 192. Handling Loading, Error, and Ready Status
+// nastavi od 194. Displaying Questions

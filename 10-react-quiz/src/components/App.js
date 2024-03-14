2602e91c95936c9730642c387780b4c6cd06b3e0
Stefan Meza <stefanmeza93@gmail.com>
2024-03-14 22:56:22 +0100

    Add progress bar; Add current question and current points

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -1,15 +1,16 @@
-import { useReducer, useEffect } from "react";
-import Header from "./Header";
-import Main from "./Main";
-import Loader from "./Loader";
-import Error from "./Error";
-import StartScreen from "./StartScreen";
-import Question from "./Question";
-import NextButton from "./NextButton";
+import { useReducer, useEffect } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
-  status: "loading",
+  status: 'loading',
   index: 0,
   answer: null,
   points: 0,
@@ -17,67 +18,63 @@ const initialState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "dataReceived":
-      return { ...state, questions: action.payload, status: "ready" };
-    case "dataFailed":
-      return { ...state, status: "error" };
-    case "start":
-      return { ...state, status: "active" };
-    case "newAnswer":
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
+    case 'newAnswer':
       const question = state.questions.at(state.index);
       return {
         ...state,
         answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : 0,
+        points: action.payload === question.correctOption ? state.points + question.points : 0,
       };
-    case "nextQuestion":
+    case 'nextQuestion':
       return { ...state, index: state.index + 1, answer: null };
     default:
-      throw new Error("Unknown action: " + action.type);
+      throw new Error('Unknown action: ' + action.type);
   }
 };
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions?.length;
+  const maxPossiblePoints = questions.reduce((acc, cur) => acc + cur.points, 0);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch("http://localhost:8000/questions");
+        const response = await fetch('http://localhost:8000/questions');
         const data = await response.json();
 
-        dispatch({ type: "dataReceived", payload: data });
+        dispatch({ type: 'dataReceived', payload: data });
       } catch (error) {
-        dispatch({ type: "dataFailed" });
+        dispatch({ type: 'dataFailed' });
       }
     }
     fetchData();
   }, []);
 
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
       <Main>
-        {status === "loading" && <Loader />}
-        {status === "error" && <Error />}
-        {status === "ready" && (
-          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
-        )}
-        {status === "active" && (
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === 'active' && (
           <>
-            <Question
-              question={questions[index]}
-              dispatch={dispatch}
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
               answer={answer}
             />
+            <Question question={questions[index]} dispatch={dispatch} answer={answer} />
             <NextButton dispatch={dispatch} answer={answer} />
           </>
         )}
@@ -85,3 +82,5 @@ export default function App() {
     </div>
   );
 }
+
+// nastavi od finishing the quiz
--- 10-react-quiz/src/components/Progress.js
+++ 10-react-quiz/src/components/Progress.js
@@ -1,11 +1,15 @@
-function Progress() {
+function Progress({ index, numQuestions, points, maxPossiblePoints, answer }) {
   return (
-    <header className="progress">
-      <p></p>
+    <header className='progress'>
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints} points
+      </p>
     </header>
   );
 }
 
 export default Progress;
-
-// nastavi od Displaying Progress - 01:00

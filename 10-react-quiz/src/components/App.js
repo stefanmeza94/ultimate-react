0803d339f6f3199b644926c9643a499ae7a9f809
Stefan Meza <stefanmeza93@gmail.com>
2024-04-09 16:37:26 +0200

    Add highscore functionality

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -1,31 +1,32 @@
-import { useReducer, useEffect } from 'react';
-import Header from './Header';
-import Main from './Main';
-import Loader from './Loader';
-import Error from './Error';
-import StartScreen from './StartScreen';
-import Question from './Question';
-import NextButton from './NextButton';
-import Progress from './Progress';
-import FinishScreen from './FinishScreen';
+import { useReducer, useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
-  status: 'loading',
+  status: "loading",
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'dataReceived':
-      return { ...state, questions: action.payload, status: 'ready' };
-    case 'dataFailed':
-      return { ...state, status: 'error' };
-    case 'start':
-      return { ...state, status: 'active' };
-    case 'newAnswer':
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
       console.log(action.payload);
       const question = state.questions.at(state.index);
       return {
@@ -33,17 +34,21 @@ const reducer = (state, action) => {
         answer: action.payload,
         points: action.payload === question.correctOption ? state.points + question.points : 0,
       };
-    case 'nextQuestion':
+    case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
-    case 'finish':
-      return { ...state, status: 'finished' };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore: state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
-      throw new Error('Unknown action: ' + action.type);
+      throw new Error("Unknown action: " + action.type);
   }
 };
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions?.length;
   const maxPossiblePoints = questions.reduce((acc, cur) => acc + cur.points, 0);
@@ -51,25 +56,25 @@ export default function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('http://localhost:8000/questions');
+        const response = await fetch("http://localhost:8000/questions");
         const data = await response.json();
 
-        dispatch({ type: 'dataReceived', payload: data });
+        dispatch({ type: "dataReceived", payload: data });
       } catch (error) {
-        dispatch({ type: 'dataFailed' });
+        dispatch({ type: "dataFailed" });
       }
     }
     fetchData();
   }, []);
 
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
       <Main>
-        {status === 'loading' && <Loader />}
-        {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === 'active' && (
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === "active" && (
           <>
             <Progress
               index={index}
@@ -82,10 +87,12 @@ export default function App() {
             <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
           </>
         )}
-        {status === 'finished' && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore} />
+        )}
       </Main>
     </div>
   );
 }
 
-// nastavi od Finishing the Quiz - 11:00
+// nastavi od Restarting a Quiz
--- 10-react-quiz/src/components/FinishScreen.js
+++ 10-react-quiz/src/components/FinishScreen.js
@@ -1,20 +1,19 @@
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
-  if (percentage === 100) emoji = '🥇';
-  if (percentage >= 80 && percentage < 100) emoji = '🎉';
-  if (percentage >= 50 && percentage < 80) emoji = '😄';
-  if (percentage >= 0 && percentage < 50) emoji = '🤔';
-  if (percentage === 0) emoji = '🤦‍♂️';
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😄";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦‍♂️";
 
   return (
     <>
-      <p className='result'>
-        <span>{emoji}</span>You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-        {Math.ceil(percentage)})
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)})
       </p>
-      <p className='highscore'>(Highscore: X score)</p>
+      <p className="highscore">(Highscore: {highscore} score)</p>
     </>
   );
 }

0bf00897d1ac5fb53437da9da1a88548a3c84154
Stefan Meza <stefanmeza93@gmail.com>
2024-04-15 22:57:45 +0200

    Add css modules and implement active link for routing as well as global css class from css module

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import PageNotFound from "./pages/PageNotFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='app' element={<AppLayout />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
@@ -19,4 +21,4 @@ function App() {
 
 export default App;
 
-// nastavi od - Styling Options for React Application
+// nastavi od Building the pages
--- 11-worldwise/src/components/PageNav.jsx
+++ 11-worldwise/src/components/PageNav.jsx
@@ -1,17 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+import styles from './PageNav.module.css';
 
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to='/'>Home</NavLink>
         </li>
         <li>
-          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to='/pricing'>Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to='/product'>Product</NavLink>
         </li>
       </ul>
     </nav>

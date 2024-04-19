b5c3d3c76e5e42aa74224047b7dd783219b830ad
Stefan Meza <stefanmeza93@gmail.com>
2024-04-19 20:31:03 +0200

    Add stash

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -5,6 +5,14 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+<<<<<<< Updated upstream
+=======
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+
+const BASE_URL = "http://localhost:8000";
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -15,9 +23,16 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
+<<<<<<< Updated upstream
           <Route index element={<p>List of cities</p>} />
           <Route path="cities" element={<p>List of cities</p>} />
           <Route path="countries" element={<p>Counties</p>} />
+=======
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="countries" element={<CountryList cities={cities} />} />
+>>>>>>> Stashed changes
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />

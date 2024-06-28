25933c7b9f3e4223584dfcceaffe220eb6969116
Stefan Meza <stefanmeza93@gmail.com>
2024-06-28 09:58:40 +0200

    Add error handling

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -8,22 +8,34 @@ import Box from "./components/Box";
 import WatchedSummery from "./components/WatchedSummery";
 import Loader from "./components/Loader";
 import { KEY } from "./constants";
+import ErrorMessage from "./components/ErrorMessage";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setIsError] = useState("");
 
-  const query = "interstellar";
+  const query = "antman";
 
   useEffect(function () {
     async function fetchMovies() {
-      setIsLoading(true);
-      const response = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-      const data = await response.json();
+      try {
+        setIsLoading(true);
+        const response = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
 
-      setMovies(data.Search);
-      setIsLoading(false);
+        if (!response.ok) throw new Error("Something went wrong with fetching movies!");
+
+        const data = await response.json();
+
+        if (data.Response === "False") throw new Error("Movie not found!");
+
+        setMovies(data.Search);
+      } catch (error) {
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchMovies();
@@ -36,7 +48,11 @@ export default function App() {
         <Results movies={movies} />
       </Navbar>
       <Main>
-        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
+        </Box>
         <Box>
           <WatchedSummery watched={watched} />
         </Box>

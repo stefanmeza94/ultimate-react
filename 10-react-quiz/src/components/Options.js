8c2ef13d48aab947ad7bdf4bb8791fe28d0b0cf1
Stefan Meza <stefanmeza93@gmail.com>
2025-02-10 16:41:22 +0100

    Add question and answer; Add ability to answer the question

---
+++ 10-react-quiz/src/components/Options.js
@@ -0,0 +1,23 @@
+const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => {
+        return (
+          <button
+            key={option}
+            className={`btn btn-option ${index === answer ? "answer" : ""} ${
+              hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""
+            }`}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Options;

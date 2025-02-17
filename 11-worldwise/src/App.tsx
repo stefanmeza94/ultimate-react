75e63b541cfd391bc88bbb096aa8afd2bf67b020
Stefan Meza <stefanmeza93@gmail.com>
2025-02-17 14:06:54 +0100

    Add basic routes

---
+++ 11-worldwise/src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+// 207 - linking between routes
---
+++ 11-worldwise/src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Worldwise</h1>
+    </div>
+  );
+};
+
+export default Homepage;

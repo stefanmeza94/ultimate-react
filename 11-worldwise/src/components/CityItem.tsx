4944b8983f5265e780c675d0ab19b3abea48504f
Stefan Meza <stefanmeza93@gmail.com>
2025-02-19 15:10:16 +0100

    Add styling for city item and message if there are not cities yet

--- 11-worldwise/src/components/CityItem.tsx
+++ 11-worldwise/src/components/CityItem.tsx
@@ -1,7 +1,22 @@
 import { City } from "../App";
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
 
 const CityItem = ({ city }: { city: City }) => {
-  return <li>{city.cityName}</li>;
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{city.emoji}</span>
+      <h3 className={styles.name}>{city.cityName}</h3>
+      <time className={styles.date}>({formatDate(city.date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
 };
 
 export default CityItem;
--- 11-worldwise/src/components/CityList.tsx
+++ 11-worldwise/src/components/CityList.tsx
@@ -2,6 +2,7 @@ import styles from "./CityList.module.css";
 import { City } from "../App";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 
 type CityListProps = {
   cities: City[];
@@ -10,6 +11,9 @@ type CityListProps = {
 
 const CityList = ({ cities, isLoading }: CityListProps) => {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message="Add your first city by clicking on the city on the map" />;
+
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
